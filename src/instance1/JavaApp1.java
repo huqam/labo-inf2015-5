7e06bfed9632a105a1433e4be59a18d88e7692d2
Hugo <hugo@example.com>
2014-02-06 21:45:34 -0500

    ins2 msg 5

--- src/instance1/JavaApp1.java
+++ src/instance1/JavaApp1.java
@@ -16,7 +16,7 @@ public class JavaApp1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("2");
+        System.out.println("5");
     }
     
 }
